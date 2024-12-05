7be1a9c35892c0768f0c918dcd4200dbc5c1e35d
Neil <neile@live.com>
2024-12-05 05:28:05 -0800

    Add code comment

--- ViewModels/ModelMatching/ConnectCommand.cs
+++ ViewModels/ModelMatching/ConnectCommand.cs
@@ -1,5 +1,7 @@
 using System.Windows.Input;
 
+// This method of adding commands to view models comes from
+// https://learn.microsoft.com/en-us/archive/msdn-magazine/2009/february/patterns-wpf-apps-with-the-model-view-viewmodel-design-pattern#id0090051
 namespace vmr_generator.ViewModels.ModelMatching
 {
 	public partial class ModelMatchingViewModel
